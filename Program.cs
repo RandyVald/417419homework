0b1345cd57c6a41ba6ce38492e7c9ed19e7d4966
RandyVald <radionburda@gmail.com>
2024-03-17 19:42:45 +0300

    add void PrintArray

--- Program.cs
+++ Program.cs
@@ -1,14 +1,4 @@
 ﻿
-// Задача: Написать программу, которая из имеющегося массива строк формирует новый массив 
-// из строк, длина которых меньше, либо равна 3 символам. 
-// Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения 
-// алгоритма. При решении не рекомендуется пользоваться коллекциями, 
-// лучше обойтись исключительно массивами.
-// Примеры:
-// [“Hello”, “2”, “world”, “:-)”] → [“2”, “:-)”]
-// [“1234”, “1567”, “-2”, “computer science”] → [“-2”]
-// [“Russia”, “Denmark”, “Kazan”] → []
-
 int GetCount(string[] Array)
 {
     int count = 0;
@@ -38,6 +28,17 @@ string[] CreatNewArr(string[] Array, int count)
     return NewArr;
 }
 
+void PrintArray(string[] Array)
+
+{
+    for (int i = 0; i < Array.Length; i++)
+
+    {
+
+        Console.Write($"{Array[i]} ");
+
+    }
+}
 
 
 Console.Clear();
@@ -45,5 +46,4 @@ Console.Clear();
 string[] Array = ["Hello", "2", "world", ":-)"];
 int Count = GetCount(Array);
 string[] NewArr = CreatNewArr(Array, Count);
-
-System.Console.WriteLine(Count);
+PrintArray(NewArr);
